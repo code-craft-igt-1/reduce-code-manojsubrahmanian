6307996387f485c5df2ec234dd522dd735070b78
Manoj Subrahmanian <manoj.subramanian@gmail.com>
2024-12-04 17:05:36 +0530

    Draft

--- image.h
+++ image.h
@@ -1,20 +1,23 @@
 #pragma once
 #include <cstdint>
-#include <iostream>
+#include <memory>
+#include <functional>
+#include <vector>
 
 class Image {
  public:
-    Image(uint16_t rows, uint16_t columns): m_rows(rows), m_columns(columns) {
-        pixels = new uint8_t[m_rows * m_columns];
+    int m_rows;
+    int m_columns;
+    std::vector<uint8_t> pixels;
+
+    // Constructor
+    Image(int rows, int columns) : m_rows(rows), m_columns(columns), pixels(rows * columns) {}
+
+    void processPixels(const std::function<void(int, int)>& processor) {
+        for (int x = 0; x < m_rows; x++) {
+            for (int y = 0; y < m_columns; y++) {
+                processor(x, y);
+            }
+        }
     }
-    ~Image() {
-        std::cout << "freeing pixel memory.\n";
-        delete[] pixels;
-    }
-    bool imageSizeIsValid() {
-        return m_rows <= 1024 && m_columns <= 1024;
-    }
-    const uint16_t m_rows;
-    const uint16_t m_columns;
-    uint8_t* pixels;  // max 1k x 1k image
 };
